805c0c576702568e779fbf3500eda80fd507cee2
feasible-creative <feasible.creative@gmail.com>
2022-09-25 15:16:34 -0400

    Update JavaScript to output equally sized squares based on userinput to button click event

--- index.js
+++ index.js
@@ -6,8 +6,44 @@ function setGrid(){
     answer = prompt("How many squares per side? (Max 100)")
     // console.log(`"${answer}"`);
     gridContainer.style.gridTemplateColumns = `repeat(${answer},1fr)`;
+
+
+
+    for (i = 1; i <= (answer**2); i++) {
+      const grid16 = document.createElement("div");
+      grid16.textContent = "";
+
+
+      // grid16.style.width = `${answer}` + "px";
+      // grid16.style.height = `${answer}` + "px";
+
+
+        
+      gridContainer.appendChild(grid16);
+
+      grid16.classList.add("gridItem");
+
+      grid16.addEventListener("mouseover", () => {
+        // grid16.style.backgroundColor = "black"
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        grid16.style.backgroundColor = "#" + randomColor;
+
+        grid16.style.backgroundColor = "randomColor";
+      });
+    }
+
+
+
+
+
   })
 }
+
+
+
+
+
+
 setGrid()
 
 // function playGame() {
