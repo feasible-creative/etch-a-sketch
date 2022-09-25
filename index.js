40e26fc8b20f0cbcb3dee1b3baa641f14d166165
feasible-creative <feasible.creative@gmail.com>
2022-09-25 11:25:07 -0400

    Update CSS properties, push user prompt response after click evevent listener to set amount of columns

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
+const button = document.querySelector("#squareCount");
+const gridContainer = document.getElementById("gridContainer")
 
-
-for (i = 1; (i <= 100); i++) {
-  const grid16 = document.createElement("div");
-  grid16.textContent = "";
-  grid16.style.width = "10%"
-  grid16.style.height = "3rem"
-  
-
-  
-
-  const gridContainer = document.getElementById("gridContainer").appendChild(grid16);
-  grid16.classList.add('gridItem')
-
-  
-  
-grid16.addEventListener('mouseover', ()=>{
-  // grid16.style.backgroundColor = "black"
-  const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  grid16.style.backgroundColor = "#" + randomColor;
-  
-  grid16.style.backgroundColor = "randomColor"
-
-
-
-})
-
-
-
+function setGrid(){
+  button.addEventListener('click', () => {
+    answer = prompt("How many squares per side? (Max 100)")
+    // console.log(`"${answer}"`);
+    gridContainer.style.gridTemplateColumns = `${answer}`;
+  })
 }
+setGrid()
+
+// function playGame() {
+//   button.addEventListener("click", () => {
+//     answer = prompt("How many squares per side? (Max 100)");
+//     console.log(answer);
+//     if (answer > 100) {
+//       alert("ERROR: Please select a value less than 100.");
+//     }
+
+//     for (i = 1; i <= 100; i++) {
+//       const grid16 = document.createElement("div");
+//       grid16.textContent = "";
 
 
-const button = document.querySelector('#squareCount')
-button.addEventListener('click', () =>{
-  answer = prompt('How many squares per side? (Max 100)')
-  console.log(answer)
-  if (answer > 100){
-    alert("ERROR: Please select a value less than 100.")
-  }
+//       // grid16.style.width = `${answer}` + "px";
+//       // grid16.style.height = `${answer}` + "px";
 
 
-})
+        
+//       gridContainer.appendChild(grid16);
 
-// console.log(i);
-// console.log('hi')
+//       grid16.classList.add("gridItem");
+
+//       grid16.addEventListener("mouseover", () => {
+//         // grid16.style.backgroundColor = "black"
+//         const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+//         grid16.style.backgroundColor = "#" + randomColor;
+
+//         grid16.style.backgroundColor = "randomColor";
+//       });
+//     }
+//   });
+// }
+
+// playGame();
+
+// // console.log(i);
+// // console.log('hi')
